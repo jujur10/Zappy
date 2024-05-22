e705eda8310a7bf4a51cbc41468cdbd85a77e9fa
Julien ROIRON <julien.roiron@epitech.eu>
2024-05-22 16:50:10 +0200

    feat: complete argument parsing implementation with unit tests

--- server/src/main.c
+++ server/src/main.c
@@ -5,9 +5,30 @@
 ** Zappy server
 */
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+#include "arguments.h"
+
+static uint8_t print_help(void)
+{
+    write(1, "USAGE: ./zappy_server -p port -x width -y height -n name1 "
+    "name2 ... -c clientsNb -f freq\n", 89);
+    return 0;
+}
 
 int main(int argc, char *argv[])
 {
-    printf("Hello World\n");
+    argument_t args = {};
+
+    if (argc < MIN_NB_OF_ARGS) {
+        if (2 == argc && 0 == memcmp(argv[1], "-help", 6))
+            return print_help();
+        return 84;
+    }
+    if (1 == get_arguments(argc, (const char **)argv, &args))
+        return 84;
+    free(args.team_names);
     return 0;
 }
---
+++ server/tests/tests_parsing.c
@@ -0,0 +1,45 @@
+/*
+** EPITECH PROJECT, 2024
+** tests_seminar.cpp
+** File description:
+** Tests.
+*/
+#include <unistd.h>
+#include <criterion/criterion.h>
+
+#include "arguments.h"
+
+Test(TEST_PARSING, test_parsing_01)
+{
+    char *argv[] = {"/bin/ls", "-c", "90", "-x", "40", "-y", "50", "-p",
+    "500", "-f", "2", "-n", "toto", "titi", "tutu"};
+    uint32_t argc = 15;
+    argument_t args = {};
+    uint8_t return_status;
+
+    return_status = get_arguments(argc, (const char **)argv, &args);
+    cr_assert_eq(return_status, 0, "Parsing");
+    cr_assert_eq(args.port, 500, "Port");
+    cr_assert_eq(args.width, 40, "Width");
+    cr_assert_eq(args.height, 50, "Height");
+    cr_assert_eq(args.clients_nb, 90, "Clients nb value");
+    cr_assert_eq(args.frequency, 2, "Frequency");
+    cr_assert_eq(args.nb_of_teams, 3, "Number of teams");
+    cr_assert_str_eq(args.team_names[0], "toto", "Toto team name");
+    cr_assert_str_eq(args.team_names[1], "titi", "Titi team name");
+    cr_assert_str_eq(args.team_names[2], "tutu", "Tutu team name");
+    free(args.team_names);
+}
+
+Test(TEST_PARSING, test_parsing_02)
+{
+    char *argv[] = {"/bin/ls", "-c", "-10", "-x", "40", "-y", "50", "-p",
+    "500", "-f", "2", "-n", "toto", "titi", "tutu"};
+    uint32_t argc = 15;
+    argument_t args = {};
+    uint8_t return_status;
+
+    return_status = get_arguments(argc, (const char **)argv, &args);
+    cr_assert_eq(return_status, 1, "Bad parsing");
+    free(args.team_names);
+}
