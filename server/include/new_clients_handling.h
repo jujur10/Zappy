bb5a5240867e213b0662680206f288d579a3d12e
Julien ROIRON <julien.roiron@epitech.eu>
2024-06-01 19:10:06 +0200

    fix: client connection/disconnection

--- server/include/new_clients_handling.h
+++ server/include/new_clients_handling.h
@@ -16,10 +16,13 @@
 /// @param client The client pointer we want to initialize.
 void init_new_client(server_t PTR server, new_client_t PTR client);
 
-/// @brief Function which destroy a client.
+/// @brief Function which destroy a client.\n
+/// - Close the client socket properly.
+/// - Move the last element to the index we want to destroy, and set to zero\n
+/// the old position of the last element.
 /// @param server The server pointer.
-/// @param client The client pointer we want to destroy.
-void destroy_new_client(server_t PTR server, new_client_t PTR client);
+/// @param client_idx The client index we want to destroy.
+void destroy_new_client(server_t PTR server, uint32_t client_idx);
 
 /// @brief Function called when the server receive data from a new_client_t.\n
 /// - Put the client's message into a buffer.\n
@@ -28,8 +31,8 @@ void destroy_new_client(server_t PTR server, new_client_t PTR client);
 /// - If the team doesn't exist, destroy the client.\n
 /// - If the team exists, respond "ok\\n" to the client.
 /// @param server The server pointer.
-/// @param client The pointer to the client who sent the message.
-void on_new_client_rcv(server_t PTR server, new_client_t PTR client);
+/// @param client The client index of the client who sent the message.
+void on_new_client_rcv(server_t PTR server, uint32_t client_idx);
 
 /// @brief Function called when a new_client_t is communicating with the
 /// server.\n
--- server/src/new_clients_handling.c
+++ server/src/new_clients_handling.c
@@ -11,6 +11,7 @@
 
 #include "server.h"
 #include "clock.h"
+#include "new_clients_handling.h"
 
 void init_new_client(server_t PTR server, new_client_t PTR client)
 {
@@ -21,29 +22,32 @@ void init_new_client(server_t PTR server, new_client_t PTR client)
     AUTH_TIMEOUT_NS);
 }
 
-void destroy_new_client(server_t PTR server, new_client_t PTR client)
+void destroy_new_client(server_t PTR server, uint32_t client_idx)
 {
-    FD_CLR(client->sock, &server->current_socks);
-    close(client->sock);
-    memset(client, 0, sizeof(new_client_t));
+    FD_CLR(server->clients[client_idx].sock, &server->current_socks);
+    close(server->clients[client_idx].sock);
+    memmove(&server->clients[client_idx],
+    &server->clients[server->nb_clients - 1], sizeof(new_client_t));
+    memset(&server->clients[server->nb_clients - 1], 0, sizeof(new_client_t));
     server->nb_clients--;
 }
 
-void on_new_client_rcv(server_t PTR server, new_client_t PTR client)
+void on_new_client_rcv(server_t PTR server, uint32_t client_idx)
 {
     static char buffer[64];
-    int64_t bytes_received = recv(client->sock, buffer, sizeof(buffer), 0);
+    int64_t bytes_received = recv(server->clients[client_idx].sock, buffer,
+    sizeof(buffer), 0);
     uint64_t client_name_length;
     int32_t team_index;
 
     if (bytes_received < 1)
-        destroy_new_client(server, client);
+        destroy_new_client(server, client_idx);
     client_name_length = strcspn(buffer, "\n");
     team_index = get_team_index_by_name(server->teams,
     server->args->nb_of_teams, buffer, (uint32_t)client_name_length);
     if (-1 == team_index)
-        destroy_new_client(server, client);
-    send(client->sock, "ok\n", 3, 0);
+        destroy_new_client(server, client_idx);
+    send(server->clients[client_idx].sock, "ok\n", 3, 0);
 }
 
 /// @brief Check the status of the client.
@@ -68,21 +72,21 @@ static uint8_t is_ready(server_t PTR server, new_client_t PTR client,
 /// @brief Function which check and handle if the client's socket waiting to
 /// be read.
 /// @param server The server pointer.
-/// @param client The client we want to check.
+/// @param client_idx The index of the client we want to check.
 /// @param rfds The read file descriptor set.
 /// @param select_ret The select return value to decrement if an action is
 /// made.
 /// @return 1 : If an action has been made. 0 : If nothing has been made.
-static uint8_t handle_client_rfds(server_t PTR server, new_client_t PTR client,
+static uint8_t handle_client_rfds(server_t PTR server, uint32_t client_idx,
     const fd_set PTR rfds, int32_t PTR select_ret)
 {
-    switch (is_ready(server, client, rfds)) {
+    switch (is_ready(server, &server->clients[client_idx], rfds)) {
         case 0:
-            destroy_new_client(server, client);
+            destroy_new_client(server, client_idx);
             (*select_ret)--;
             return 1;
         case 1:
-            on_new_client_rcv(server, client);
+            on_new_client_rcv(server, client_idx);
             (*select_ret)--;
             return 1;
         default:
@@ -93,17 +97,17 @@ static uint8_t handle_client_rfds(server_t PTR server, new_client_t PTR client,
 /// @brief Function which check and handle if the client's socket waiting to
 /// be write.
 /// @param server The server pointer.
-/// @param client The client we want to check.
+/// @param client_idx The index of the client we want to check.
 /// @param wfds The write file descriptor set.
 /// @param select_ret The select return value to decrement if an action is
 /// made.
 /// @return 1 : If an action has been made. 0 : If nothing has been made.
-static uint8_t handle_client_wfds(server_t PTR server, new_client_t PTR client,
+static uint8_t handle_client_wfds(server_t PTR server, uint32_t client_idx,
     const fd_set PTR wfds, int32_t PTR select_ret)
 {
-    switch (is_ready(server, client, wfds)) {
+    switch (is_ready(server, &server->clients[client_idx], wfds)) {
         case 0:
-            destroy_new_client(server, client);
+            destroy_new_client(server, client_idx);
             (*select_ret)--;
             return 1;
         case 1:
@@ -124,11 +128,10 @@ void handle_new_clients(server_t PTR server, const fd_set PTR rfds,
         if (0 == server->clients[i].sock)
             continue;
         count_client++;
-        if (1 == handle_client_rfds(server, &server->clients[i], rfds,
+        if (1 == handle_client_rfds(server, i, rfds,
         select_ret))
             continue;
-        if (1 == handle_client_wfds(server, &server->clients[i], wfds,
-        select_ret))
+        if (1 == handle_client_wfds(server, i, wfds, select_ret))
             continue;
     }
 }
