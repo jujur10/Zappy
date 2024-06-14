e987ddc2d1eb5c840219e9b1723df6c8c93884db
quentinf <quentin.filliaert@epitech.eu>
2024-06-14 09:49:37 +0200

    feat: Improve network loop to call parsing functions

--- gui/src/network_main_loop.cpp
+++ gui/src/network_main_loop.cpp
@@ -6,30 +6,32 @@
 #include <my_write.hpp>
 
 #include "networking.hpp"
+#include "server_commands.hpp"
 #include "to_gui_commands.hpp"
 #include "to_server_command.hpp"
-#include "server_commands.hpp"
 
 namespace zappy_gui::net
 {
 void HandleGuiCommand(const Socket& serverSocket)
 {
     char* request = nullptr;
-    while (!GuiToServerQueue.try_pop(request) && request != nullptr)
+    while (GuiToServerQueue.try_pop(request) && request != nullptr)
     {
         serverSocket.Write(request, std::strlen(request));
     }
 }
 
-void HandleServerCommand(const Socket& serverSocket, std::string& line)
+void HandleServerCommand(const std::string& line)
 {
-    const uint32_t command = *std::bit_cast<const uint32_t *>(line.c_str()) & 0x00'FF'FF'FFu;
+    const uint32_t command =
+        *std::bit_cast<const uint32_t*>(line.c_str()) & 0x00'FF'FF'FFu;
 
     switch (static_cast<ServerCommands>(command))
     {
         case ServerCommands::MAP_TILE:
         {
-            ParseTileUpdateCommand(static_cast<std::string_view>(line).substr(3));
+            ParseTileUpdateCommand(
+                static_cast<std::string_view>(line).data() + 4);
         }
         default:
             return;
@@ -40,12 +42,16 @@ void NetworkTreadLoop(const std::stop_token& stoken, const Socket& serverSocket)
 {
     const FileWriter errWriter(2);
     std::vector<char> responseBuffer;
-    std::string errorMsg;
 
     while (true)
     {
         HandleGuiCommand(serverSocket);
-        serverSocket.ReadLineTimeout(responseBuffer, 10, errorMsg);
+
+        std::string responseLine;
+        while (!(responseLine = serverSocket.ReadLineFast(responseBuffer)).empty())
+        {
+            HandleServerCommand(responseLine);
+        }
         if (stoken.stop_requested())
         {
             errWriter.writeNoReturn("Stop requested\n", 15);
