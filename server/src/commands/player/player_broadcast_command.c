8b13fd65d9b91cff44b7ab93a1b763ba9854af08
Julien ROIRON <julien.roiron@epitech.eu>
2024-06-20 00:11:45 +0200

    feat: pbc player event implementation

--- server/src/commands/player/player_broadcast_command.c
+++ server/src/commands/player/player_broadcast_command.c
@@ -4,8 +4,10 @@
 ** File description:
 ** player_broadcast_command.c.
 */
+#include <string.h>
 #include "server.h"
 #include "utils/itoa/fast_itoa.h"
+#include "commands/command_utils.h"
 
 /// @brief There is always 8 cells around a point in a 2d array.
 static const int32_t NB_OF_CELLS_AROUND_CENTER = 8;
@@ -93,6 +95,45 @@ static void broadcast_message_to_player(player_t PTR player,
     add_msg_to_queue(&player->queue, &message);
 }
 
+/// @brief Function which sends to GUIs the events of pbc.
+///
+/// @param server The server structure.
+/// @param message_buffer The message buffer.
+/// @param player_idx The player index.
+static void send_pbc_to_guis(server_t PTR server,
+    const buffer_t PTR message_buffer, uint32_t player_idx)
+{
+    msg_t message;
+    char msg_content[4 + UINT32_MAX_DIGITS + 1] = "pbc ";
+    uint32_t count = 4;
+    const player_t *player = &server->players[player_idx];
+    string_t string;
+
+    write_nb_to_buffer(player->sock, msg_content, &count);
+    init_string_from_chars(&string, msg_content, count);
+    append_to_string_from_chars(&string, message_buffer->ptr + sizeof
+    (MESSAGE_STR) - 1, message_buffer->len - sizeof(MESSAGE_STR) + 1);
+    for (uint16_t i = 0; i < server->nb_guis; i++) {
+        create_message(string.ptr, string.len, &message);
+        add_msg_to_queue(&server->guis[i].queue, &message);
+    }
+    clear_string(&string);
+}
+
+/// @brief Function which re-initialize the broadcast buffer.
+/// - Reinitialize.
+/// - Append the message.
+///
+/// @param buffer The buffer to re-initialize.
+/// @param command The player's command (containing the message).
+static void reinitialize_broadcast_buffer(buffer_t PTR buffer,
+    const player_command_t PTR command)
+{
+    buffer->len = sizeof(MESSAGE_STR) - 1;
+    append_to_buffer_from_chars(buffer, command->argument.ptr,
+        command->argument.len);
+}
+
 void execute_player_broadcast_command(server_t PTR server, uint16_t player_idx,
     const player_command_t PTR command)
 {
@@ -103,9 +144,8 @@ void execute_player_broadcast_command(server_t PTR server, uint16_t player_idx,
     int32_t direction;
     buffer_t *buffer = &server->generated_buffers.buffers[PRE_BROADCAST_MSG];
 
-    buffer->len = sizeof(MESSAGE_STR) - 1;
-    append_to_buffer_from_chars(buffer, command->argument.ptr,
-        command->argument.len);
+    reinitialize_broadcast_buffer(buffer, command);
+    send_pbc_to_guis(server, buffer, player_idx);
     for (uint32_t i = 0; i < server->nb_players; i++) {
         if (i == player_idx)
             continue;
