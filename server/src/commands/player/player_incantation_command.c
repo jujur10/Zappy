2098c4f229c332cdd14baec72443025c9081d0a9
Julien ROIRON <julien.roiron@epitech.eu>
2024-06-19 23:37:38 +0200

    feat: pic player event implementation

--- server/src/commands/player/player_incantation_command.c
+++ server/src/commands/player/player_incantation_command.c
@@ -8,6 +8,8 @@
 #include "resources.h"
 #include "game_settings.h"
 #include "commands/player_commands.h"
+#include "commands/command_utils.h"
+#include "utils/itoa/fast_itoa.h"
 
 /// @brief Array of resources_t representing the needed elements for elevation.
 const resources_t elevation_requirements[] = {
@@ -106,6 +108,56 @@ bool verify_requirements(server_t PTR server, uint16_t player_idx)
     return false;
 }
 
+/// @brief Function used to write the player indexes (sockets) into the string.
+///
+/// @param server The server structure.
+/// @param player_idx The player index.
+/// @param string The string to write to.
+static void write_ready_players_into_string(server_t PTR server,
+    uint16_t player_idx, string_t PTR string)
+{
+    const player_t *player = &server->players[player_idx];
+    char msg_content[4 + (3 * UINT32_MAX_DIGITS) + 1] = "pic ";
+    uint32_t count;
+
+    for (uint32_t i = 0; i < server->nb_players; i++) {
+        if (i != player_idx && true == is_coordinates_equal
+        (&player->coordinates, &server->players[i].coordinates) &&
+        player->level == server->players[i].level) {
+            count = (uint32_t)fast_itoa_u32(server->players[i].sock,
+                msg_content);
+            msg_content[count] = ' ';
+            append_to_string_from_chars(string, msg_content, count + 1);
+        }
+    }
+    string->ptr[string->len - 1] = '\n';
+}
+
+/// @brief Function which sends to GUIs the events of pic.
+///
+/// @param server The server structure.
+/// @param team The player's team.
+/// @param player_idx The player index.
+static void send_pic_to_guis(server_t PTR server, uint32_t player_idx)
+{
+    msg_t message;
+    char msg_content[4 + (3 * UINT32_MAX_DIGITS) + 1] = "pic ";
+    uint32_t count = 4;
+    const player_t *player = &server->players[player_idx];
+    string_t string;
+
+    write_nb_to_buffer(player->coordinates.x, msg_content, &count);
+    write_nb_to_buffer(player->coordinates.y, msg_content, &count);
+    write_nb_to_buffer(player->level + 1, msg_content, &count);
+    init_string_from_chars(&string, msg_content, count);
+    write_ready_players_into_string(server, (uint16_t)player_idx, &string);
+    for (uint16_t i = 0; i < server->nb_guis; i++) {
+        create_message(string.ptr, string.len, &message);
+        add_msg_to_queue(&server->guis[i].queue, &message);
+    }
+    clear_string(&string);
+}
+
 void execute_player_incantation_command(server_t PTR server,
     uint16_t player_idx, UNUSED const player_command_t PTR command)
 {
@@ -113,6 +165,7 @@ void execute_player_incantation_command(server_t PTR server,
     player_t *player = &server->players[player_idx];
 
     if (true == verify_requirements(server, player_idx)) {
+        send_pic_to_guis(server, player_idx);
         create_message("Elevation underway\n", 19, &message);
         message.event.player_event = PLAYER_EVENT_INCANTATION;
         add_msg_to_queue(&player->queue, &message);
